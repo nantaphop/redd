578f0f7899b02ab09973be892e591ebc00d4ed60
Nantaphop <nantaphop.p@gmail.com>
2017-10-18 10:54:45 +0700

    Update Header Layout Style

--- app/components/Header.js
+++ app/components/Header.js
@@ -12,7 +12,7 @@ import List, { ListItem, ListItemText } from 'material-ui/List';
 import Menu, { MenuItem } from 'material-ui/Menu';
 import SortRedditButton from './SortRedditButton'
 import UserMenuButton from './UserMenuButton'
-
+import Grid from 'material-ui/Grid'
 
 const enhance = compose(
     inject('viewStore'),
@@ -32,28 +32,49 @@ const Title = styled(Typography) `
 const SubredditTitle = styled(Typography) `
     margin-right: 16px !important;
 `
-const Right = styled.div`
+const Left = styled.div`
+    display: flex;
+    align-items: center;
+    height: 100%;
+`
+const Center = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+`
+const Right = styled.div`
     display: flex;
     justify-content: flex-end;
 `
 export default enhance((props) => (
     <AppBar color="primary">
         <Toolbar>
-            <Title type="title" color="inherit" >
-                Redd
-            </Title>
-            <SubredditTitle type="title" color="inherit" >
-                {props.subredditStore.subreddit ? `/r/${props.subredditStore.subreddit}` : 'Front Page'}
-            </SubredditTitle>
-            <SortRedditButton />
-            <Right>
-                {
-                    props.viewStore.currentUser
-                        ? <UserMenuButton />
-                        : <Button color="contrast" onClick={props.handleLogin}>Login</Button>
-                }
-            </Right>
+            <Grid container spacing={16}>
+                <Grid item md={3}>
+                    <Left>
+                        <Title type="title" color="inherit" >
+                            Redd
+                        </Title>
+                    </Left>
+                </Grid>
+                <Grid item md={4}>
+                    <Center>
+                        <SubredditTitle type="title" color="inherit" >
+                            {props.subredditStore.subreddit ? `/r/${props.subredditStore.subreddit}` : 'Front Page'}
+                        </SubredditTitle>
+                        <SortRedditButton />
+                    </Center>
+                </Grid>
+                <Grid item md={5}>
+                    <Right>
+                        {
+                            props.viewStore.currentUser
+                                ? <UserMenuButton />
+                                : <Button color="contrast" onClick={props.handleLogin}>Login</Button>
+                        }
+                    </Right>
+                </Grid>
+            </Grid>
         </Toolbar>
     </AppBar>
 ))
