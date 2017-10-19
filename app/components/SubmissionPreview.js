0062314e796d4786483f77373808f05a09773ea1
Nantaphop <nantaphop.p@gmail.com>
2017-10-19 21:31:30 +0700

    refactor submission preview

--- app/components/SubmissionPreview.js
+++ app/components/SubmissionPreview.js
@@ -1,39 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
 import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
-import { compose } from 'recompose'
+import { compose, withPropsOnChange } from 'recompose'
 
 
 type SubmissionPreviewProps = {
-    submission: object
+    submission: object,
+    preview?: string,
+    previewType?: string
 }
 
-const enhance = compose()
+const enhance = compose(
+    withPropsOnChange(['submission'], props => {
+        let { submission } = props
+        let preview
+        if (submission.preview && submission.preview.images) {
+            let resolutions: object[] = submission.preview.images[0].resolutions
+            let heightMoreThan200 = resolutions.filter(r => r.height > 200)
+            preview = heightMoreThan200[0] && heightMoreThan200[0].url
+        }
+        let previewType
+        let gifUrl
+        if (submission.url.endsWith('.gif') || submission.url.endsWith('.gifv')) {
+            previewType = 'gif'
+            gifUrl = submission.url
+            if (gifUrl.indexOf('imgur') > 0 && gifUrl.endsWith('.gifv')) {
+                gifUrl = gifUrl.substring(0, gifUrl.length - 1)
+            }
+        } else {
+            previewType = 'image'
+        }
+        return {
+            preview,
+            previewType,
+        }
+    }),
+)
 
 const Image = styled(CardMedia) `
 height: 300px;
 `
 
-export default (props: SubmissionPreviewProps) => {
-    let { submission } = props
-    console.log(submission.thumbnail, submission)
-    let preview
-    if (submission.preview && submission.preview.images) {
-        let resolutions: object[] = submission.preview.images[0].resolutions
-        let heightMoreThan200 = resolutions.filter(r => r.height > 200)
-        preview = heightMoreThan200[0] && heightMoreThan200[0].url
-    }
-    let previewType, gifUrl
-    if (submission.url.endsWith('.gif') || submission.url.endsWith('.gifv')) {
-        previewType = 'gif'
-        gifUrl = submission.url
-        if (gifUrl.indexOf('imgur') > 0 && gifUrl.endsWith('.gifv')) {
-            gifUrl = gifUrl.substring(0, gifUrl.length - 1)
-        }
-    } else {
-        previewType = 'image'
-    }
-    if (!preview) {
+export default enhance((props: SubmissionPreviewProps) => {
+    let { preview, previewType } = props
+    if (!preview){
         return null
     }
     return (
@@ -45,4 +55,4 @@ export default (props: SubmissionPreviewProps) => {
             />
         </div>
     )
-}
+})
